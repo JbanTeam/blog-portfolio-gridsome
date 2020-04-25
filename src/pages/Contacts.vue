b3da1da30a27e4bf964500a1789d29aa1d8bd181
JbanTeam <JbanTeam@yandex.ru>
2020-04-25 16:04:00 +0300

    Gridsome init

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <h1>My contacts</h1>
+    <p>Write to me</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+metaInfo: {
+    title: 'Contacts'
+  }
+}
+</script>
+
+<style>
+
+</style>
